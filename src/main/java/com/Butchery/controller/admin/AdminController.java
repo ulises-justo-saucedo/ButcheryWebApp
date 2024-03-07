fae0cd679afe1cec436512e873baf0cfe1e8a00f
Ulises Justo Saucedo <ulisesjustosaucedo@gmail.com>
2024-03-07 17:55:31 -0300

    Divided responsabilities between admin controllers

--- src/main/java/com/Butchery/controller/admin/AdminController.java
+++ src/main/java/com/Butchery/controller/admin/AdminController.java
@@ -1,40 +1,13 @@
 package com.Butchery.controller.admin;
 
-import com.Butchery.dto.ClientDTO;
-import com.Butchery.entity.ClientEntity;
-import com.Butchery.service.ClientService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/Butchery/adm")
 public class AdminController {
-    @Autowired
-    private ClientService clientService;
     @GetMapping("/panel")
     public String adminpanel(){
         return "admin/adminpanel";
-    }
-    @GetMapping("/users")
-    public ModelAndView users(){
-        ModelAndView mav = new ModelAndView();
-        mav.addObject("users",clientService.selectAll());
-        mav.setViewName("admin/usersList");
-        return mav;
-    }
-    @GetMapping("/users/update")
-    public ModelAndView showUpdatePage(@RequestParam("id") long id){
-        ModelAndView mav = new ModelAndView();
-        mav.addObject("user",clientService.selectById(id));
-        mav.setViewName("admin/updateUser");
-        return mav;
-    }
-    @PostMapping("/users/update/sent")
-    public String updateUser(@ModelAttribute ClientDTO clientDTO, @RequestParam("id") long id){
-        ClientEntity clientEntity = clientDTO.toClientEntity(id);
-        clientService.saveOrUpdate(clientEntity);
-        return "redirect:/Butchery/adm/users";
     }
 }
---
+++ src/main/java/com/Butchery/controller/admin/AdminUserController.java
@@ -0,0 +1,35 @@
+package com.Butchery.controller.admin;
+import com.Butchery.dto.ClientDTO;
+import com.Butchery.entity.ClientEntity;
+import com.Butchery.service.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/Butchery/adm/user")
+public class AdminUserController {
+    @Autowired
+    private ClientService clientService;
+    @GetMapping("/list")
+    public ModelAndView userList(){
+        ModelAndView mav = new ModelAndView();
+        mav.addObject("users",clientService.selectAll());
+        mav.setViewName("admin/usersList");
+        return mav;
+    }
+    @GetMapping("/update")
+    public ModelAndView showUpdatePage(@RequestParam("id") long id){
+        ModelAndView mav = new ModelAndView();
+        mav.addObject("user",clientService.selectById(id));
+        mav.setViewName("admin/updateUser");
+        return mav;
+    }
+    @PostMapping("/update/sent")
+    public String updateUser(@ModelAttribute ClientDTO clientDTO, @RequestParam("id") long id){
+        ClientEntity clientEntity = clientDTO.toClientEntity(id);
+        clientService.saveOrUpdate(clientEntity);
+        return "redirect:/Butchery/adm/user/list";
+    }
+}
