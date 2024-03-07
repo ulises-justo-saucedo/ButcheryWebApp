78e2cbfecd7352f7bbfaa65197353d1613ffbf42
Ulises Justo Saucedo <ulisesjustosaucedo@gmail.com>
2024-03-07 12:01:28 -0300

    First state of the project

---
+++ src/main/java/com/Butchery/ButcheryApplication.java
@@ -0,0 +1,13 @@
+package com.Butchery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ButcheryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ButcheryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Butchery/ButcheryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Butchery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ButcheryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
