dd017d27f959efd00b2badeaa9d400ac38f9de65
Ulises Justo Saucedo <ulisesjustosaucedo@gmail.com>
2024-03-08 18:04:39 -0300

    Implemented basic methods on MeatService

--- src/main/java/com/Butchery/service/MeatService.java
+++ src/main/java/com/Butchery/service/MeatService.java
@@ -1,11 +1,26 @@
 package com.Butchery.service;
 
+import com.Butchery.entity.MeatEntity;
 import com.Butchery.repository.MeatRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MeatService {
     @Autowired
     private MeatRepository meatRepository;
+    public void saveOrUpdate(MeatEntity meatEntity){
+        meatRepository.save(meatEntity);
+    }
+    public void deleteById(long id){
+        meatRepository.deleteById(id);
+    }
+    public List<MeatEntity> selectAll(){
+        return meatRepository.findAll();
+    }
+    public MeatEntity selectById(long id){
+        return meatRepository.findById(id).orElse(null);
+    }
 }
